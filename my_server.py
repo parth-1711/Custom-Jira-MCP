ea66ca32aff6b026471919a4a547924fe60097eb
parth-1711 <106518009+parth-1711@users.noreply.github.com>
2025-12-15 16:32:20 +0530

    Update my_server.py

--- my_server.py
+++ my_server.py
@@ -1,3 +1,4 @@
+import os
 from fastmcp import FastMCP
 from tools import register_jira_tools, register_confluence_tools
 from dotenv import load_dotenv
@@ -19,3 +20,4 @@ if __name__ == "__main__":
         host="127.0.0.1"
     else: host = "0.0.0.0"
     mcp.run(transport="sse", host=host, port=8080)
+
